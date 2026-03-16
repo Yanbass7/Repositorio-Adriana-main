85fc48f70b10709804c694c1f284052bc6955fcf
BIG4TECH <big4techfsa@gmail.com>
2026-03-16 17:13:09 -0300

    Atualizações vercel

--- server.js
+++ server.js
@@ -1,16 +1,4 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-
-const livrosRoutes = require('./routes/livros.routes');
-const authRoutes = require('./routes/auth.routes');
-
-app.use('/api', livrosRoutes);
-app.use('/api/auth', authRoutes);
+const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
